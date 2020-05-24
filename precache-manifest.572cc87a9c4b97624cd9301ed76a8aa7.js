77b85c0eef8eb27ef496de71c07865ff2f3f83c7
Kris <kapsona@yahoo.com>
2020-05-24 11:09:31 -0700

    Updates

---
+++ precache-manifest.572cc87a9c4b97624cd9301ed76a8aa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4c3e3b28a85ed50f35cc0c57118ba1",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "598e844695cc1e4325a7",
+    "url": "/todolist/static/css/main.1528d557.chunk.css"
+  },
+  {
+    "revision": "8b11ac6639c8633f5d5c",
+    "url": "/todolist/static/js/2.b3e761db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.b3e761db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "598e844695cc1e4325a7",
+    "url": "/todolist/static/js/main.b638088c.chunk.js"
+  },
+  {
+    "revision": "1840bfcdf4ad4e037d64",
+    "url": "/todolist/static/js/runtime-main.11166eff.js"
+  }
+]);
